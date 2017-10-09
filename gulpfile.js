af7608f17b08e9aa3915cb49301b879682dd1540
XSW22502 <l>
2017-10-09 10:01:26 +0800

    完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var minjs = require('gulp-uglify');
 var concat = require('gulp-concat');
+var mincss = require('gulp-clean-css');
+var connet = require('gulp-connect');
 gulp.task('minjs',function(){
     gulp.src(['./a.js','./b.js'])
         .pipe(minjs())
@@ -11,4 +13,16 @@ gulp.task('concat',function(){
     .pipe(concat('c.js'))
     .pipe(gulp.dest('./js/'));
 })
-gulp.task('default',['minjs','concat'])
+gulp.task('mincss',function(){
+    gulp.src('style.css')
+    .pipe(mincss())
+    .pipe(gulp.dest('./css/'));
+})
+gulp.task('connect',function(){
+    gulp.src(['./js/c.js','./css/style.css'])
+         connet.server({
+             port:8080,
+             livereload:true
+         })
+})
+gulp.task('default',['minjs','concat','mincss','connect'])
